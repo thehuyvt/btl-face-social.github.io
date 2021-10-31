e7c652b25b8f735cc7c76b8ceed41e4b6a771e6f
thehuyvt <thehuyvtvp@gmail.com>
2021-10-31 16:21:59 +0700

    Huy commit

--- members/mfooter.php
+++ members/mfooter.php
@@ -1,3 +1,4 @@
 <script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../js/navbar.js"></script>
   </body>
 </html>
--- members/mheader.php
+++ members/mheader.php
@@ -8,16 +8,27 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/mheader.css">
+    <link rel="stylesheet" href="../css/responsive.css">
     <link rel="stylesheet" href="../font/themify-icons/themify-icons.css">
     <title>Face-Social</title>
   </head>
   <body>
-    <div class="header container-fluid bg-primary p-1">
-        <ul class="nav ">
-          <li><a href="./index.php" id="logo">Face-Social</a></li>
-          <li><a href="./news.php"class="icons-news"><i class="ti-desktop"></i></a></li>
-          <li><a href="./profile.php"class="icons-news"><i class="ti-user"></i></a></li>
-          <li><a href="./mesenger.php"class="icons-news"><i class="ti-email"></i></a></li>
-          <li><a href="../logout.php" class="signout">Đăng xuất</a></li>
-         </ul>
-    </div>
+  <div id="header" >
+            <ul class="nav ">
+                <li><a href="./index.php">Face-Social</a></li>
+                <li><a href="./profile.php">Profile</a></li>
+                <li><a href="./messenger.php">Messenger</a></li>
+                <li><a href="../logout.php">Sign Out</a></li>
+            </ul>
+
+            <div class="search-btn">
+              <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light" type="submit">Search</button>
+              </form>
+            </div>
+            <div id="mobile-menu" class="mobile-menu-btn">
+                <i class="menu-icon ti-menu"></i>
+            </div>
+            
+        </div>
