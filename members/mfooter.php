54bacd2f13754e1588f1af914d642fd1931beea6
thehuyvt <thehuyvtvp@gmail.com>
2021-11-01 00:21:27 +0700

    huy dep trai

--- members/mfooter.php
+++ members/mfooter.php
@@ -1,4 +1,4 @@
-<script src="../js/bootstrap.bundle.min.js"></script>
-<script src="../js/navbar.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/navbar.js"></script>
   </body>
 </html>
